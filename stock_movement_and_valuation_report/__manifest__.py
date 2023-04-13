a5712a8456729402cd5d79383e3a4888a2d9da2d
JETZERP-LTD <hello@jetzerp.com>
2023-04-13 11:43:21 +0530

    add

--- stock_movement_and_valuation_report/__manifest__.py
+++ stock_movement_and_valuation_report/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'Stock Inventory Movement Report (On Excel)',
     'version': '1.2',
     'sequence': 1,
-    'summary': 'Stock Inventory Movement And Valuation Report in Excel sheet',
+    'summary': 'Stock Inventory Movement And Valuation Report in Excel',
     'description': """  This module helps to view product movement [IN/OUT] from selected warehouse by selecting date. """,
     'category': 'Stock',
     'author': 'JETZERP',
@@ -11,17 +11,12 @@
     'depends': ['account', 'stock', 'web'],
     'data': [
         'wizards/inventory_movemet_view.xml',
-        # 'views/web_backend_css.xml',
         'reports/report.xml',
         'security/ir.model.access.csv',
     ],
     'demo': [],
-    # 'icons': {
-    #         '64': 'static/description/icon.png',
-    #     },
     'images': [
         'static/description/banner.png',
-        # 'static/description/icon.png',
     ],
     'installable': True,
     'application': True,
